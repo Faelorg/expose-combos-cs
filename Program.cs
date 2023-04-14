4c4a8d46b90fed01077e6ea313537dba04778cf5
cofeek-codes <kormyshev11@mail.ru>
2023-04-14 11:34:17 +0500

    expose combos cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+
+    public int Factorial(int n)
+    {
+        if (n == 1) return 1;
+
+        return n * Factorial(n - 1);
+    }
+
+
+    public static int CountUnique(int num)
+    {
+        List<Char> duplicates = num.ToString().ToCharArray().ToList();
+        List<Char> tmpd = new List<char>();
+        foreach (var d in duplicates)
+        {
+            if (!tmpd.Contains(d))
+            {
+                tmpd.Add(d);
+            }
+        }
+
+        return tmpd.Count;
+    }
+
+
+    private static void Main(string[] args)
+    {
+        int num = int.Parse(System.Console.ReadLine());
+
+        int length = num.ToString().ToCharArray().Length;
+        //System.Console.WriteLine(num);
+        Console.WriteLine(CountUnique(num));
+    }
+
+
+
+}
